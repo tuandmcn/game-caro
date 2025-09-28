8dea86a28dabb482af1297a59b2f4eadad4fcaf7
Tuan Do <tuandmcn@hotmail.com>
2025-09-28 23:27:56 +0700

    Bo sung ghi chú

--- Game Caro/Constance.cs
+++ Game Caro/Constance.cs
@@ -2,10 +2,14 @@
 {
     class Constance
     {
+        //Do rong cua o co
         public static int CellWidth = 35;
+        //Chieu cao cua o co
         public static int CellHeight = 35;
 
+        //So dong ban co
         public static int nRows = 22;
+        //So cot ban co
         public static int nCols = 40;
 
         public static int CountDownStep = 100;
