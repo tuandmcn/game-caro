6b5c56fc0068ac669408e52629d0e48fe2f80d39
Tuan Do <tuandmcn@hotmail.com>
2025-10-07 11:21:42 +0700

    Rich text box custom

---
+++ Game Caro/TransparentRichTextBox.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game_Caro
+{
+    class TransparentRichTextBox : RichTextBox
+    {
+        public TransparentRichTextBox()
+        {
+            // Bật chế độ double-buffering để giảm nhấp nháy khi vẽ lại
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+        }
+
+        // Ghi đè phương thức CreateParams để thêm kiểu trong suốt
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                // Thêm WS_EX_TRANSPARENT style
+                // Điều này báo cho Windows rằng control này nên trong suốt
+                cp.ExStyle |= 0x20;
+                return cp;
+            }
+        }
+
+        // Ghi đè phương thức OnPaintBackground để không vẽ nền
+        // Điều này ngăn control tự vẽ màu nền của nó
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            // Không làm gì cả để giữ nền trong suốt
+        }
+    }
+}
